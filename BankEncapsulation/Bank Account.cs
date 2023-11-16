f657973da1f44496c5af111446edfc7e6ce33922
ZanyaMyers <myerszanya@gmail.com>
2023-11-16 08:21:07 -0800

    inital commit

---
+++ BankEncapsulation/Bank Account.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class Bank_Account
+    {
+        private double balance = 0.0; 
+    
+        public void Deposit(double money)
+        {
+            balance += money;
+        }
+    
+       public void GetBalance()
+        {
+            Console.WriteLine(balance); 
+        }
+    }
+
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Bank_Account account = new Bank_Account(); // creating the bank account 
+
+            Console.WriteLine("Enter amount to deposit");
+            double userInput = double.Parse(Console.ReadLine()); 
+            account.Deposit(userInput);
+            Console.WriteLine("");
+            Console.WriteLine("Amount of money available:");
+            account.GetBalance();
         }
     }
 }
